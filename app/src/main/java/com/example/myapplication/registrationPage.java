6607bdb786ba9d9fe3c1407dc8b555370343ba3d
kvk123 <kv58265@gmail.com>
2019-10-13 20:57:38 +0530

    Registration Firebase Added

--- app/src/main/java/com/example/myapplication/registrationPage.java
+++ app/src/main/java/com/example/myapplication/registrationPage.java
@@ -1,26 +1,67 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class registrationPage extends AppCompatActivity {
 
-    Button reg;
+    EditText nameText, emailText, carText, pwdText;
+    Button btn;
+    FirebaseAuth mAuth;
+
+    public void login(View view){
+
+        startActivity(new Intent(registrationPage.this, loginPage.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
-        reg=findViewById(R.id.register);
-        reg.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+        nameText =findViewById(R.id.Name);
+        emailText = findViewById(R.id.Email);
+        carText = findViewById(R.id.Carno);
+        pwdText = findViewById(R.id.pass);
+        btn =findViewById(R.id.register);
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent register=new Intent(registrationPage.this,loginPage.class);
-                startActivity(register);
+            public void onClick(View v) {
+
+                String email = emailText.getText().toString().trim();
+                String pwd =pwdText.getText().toString().trim();
+
+
+                mAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(registrationPage.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(registrationPage.this, "You have been successfully registered", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(registrationPage.this, MainActivity.class));
+                        } else {
+
+                            Toast.makeText(registrationPage.this, "Error in registry", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
 
