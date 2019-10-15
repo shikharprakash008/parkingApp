a27357a03f67ced756d31e0049435ee8ea8ea8ce
kvk123 <kv58265@gmail.com>
2019-10-15 19:26:11 +0530

    Booking Button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void slot_book1(View view){
+
+        startActivity(new Intent(MainActivity.this,bookingSlot1.class ));
+    }
+
+    public void slot_book2(View view){
+
+        startActivity(new Intent(MainActivity.this, bookingSlot2.class));
+    }
 
 
     @Override
