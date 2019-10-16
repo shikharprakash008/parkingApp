2fca3f6107db76b47b838ad537d70c4c58c8e756
kvk123 <kv58265@gmail.com>
2019-10-16 19:51:21 +0530

    Booking Slot Added

--- app/src/main/java/com/example/myapplication/bookingSlot1.java
+++ app/src/main/java/com/example/myapplication/bookingSlot1.java
@@ -2,9 +2,21 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class bookingSlot1 extends AppCompatActivity {
+
+    public void success(View view){
+
+
+    }
+
+    public void cancel(View view){
+
+        startActivity(new Intent(bookingSlot1.this, MainActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/myapplication/bookingSlot2.java
+++ app/src/main/java/com/example/myapplication/bookingSlot2.java
@@ -2,9 +2,21 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class bookingSlot2 extends AppCompatActivity {
+
+    public void success(View view){
+
+
+    }
+
+    public void cancel(View view){
+
+        startActivity(new Intent(bookingSlot2.this, MainActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
