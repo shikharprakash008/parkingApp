98888171c61cb5e2ae3c032e75b80bbea738ad46
kvk123 <kv58265@gmail.com>
2019-10-16 21:55:48 +0530

    Amount added

--- app/src/main/java/com/example/myapplication/bookingSlot2.java
+++ app/src/main/java/com/example/myapplication/bookingSlot2.java
@@ -5,8 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class bookingSlot2 extends AppCompatActivity {
+
+    public void rbClick(View view){
+
+        int a = 50;
+        Toast.makeText(this, "You have to pay Rs." + a +  "for the slot!!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void rbClick2(View view){
+        int b = 100;
+        Toast.makeText(this, "You have to pay Rs." + b + "for the slot!!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void rbClick3(View view){
+
+        int c= 150;
+        Toast.makeText(this, "You have to pay Rs." + c + "for the slot!!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void rbClick4(View view){
+
+        int d =200;
+        Toast.makeText(this, "You have to pay Rs." + d + "for the slot!!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void rbClick5(View view){
+
+        int e = 250;
+        Toast.makeText(this, "You have to pay Rs." + e + "for the slot!!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void rbClick6(View view){
+
+        int f = 300;
+        Toast.makeText(this, "You have to pay Rs." + f + "for the slot!!", Toast.LENGTH_SHORT).show();
+
+    }
 
     public void success(View view){
 
